22f299f3f6dcd2d6c4969c38666bd92a5c90cf5f
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-05-03 09:40:30 +0200

    Fetching pokemon data from pokeapi in pokemon.js

---
+++ pokemon.js
@@ -0,0 +1,17 @@
+// Max ammout of pokemon retrieved from the api
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+let allPokemons = [];
+
+//Fetch the pokemon data using the pokeapi
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+    .then((response) => response.json())
+    .then((data) => {
+        allPokemons = data.results; 
+    });
+
