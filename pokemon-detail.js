b73cf6e0874f408120079eca297b0fca0dd53b64
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-05-04 17:15:18 +0200

    finishing the detail page

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -1,274 +1,253 @@
 let currentPokemonId = null;
-const MAX_POKEMONS = 151;
 
-document.addEventListener("DOMContentLoaded", () => {    
-    const pokemonId = new URLSearchParams(window.location.search).get("id");
-    const id = parseInt(pokemonId, 10);
+document.addEventListener("DOMContentLoaded", () => {
+  const MAX_POKEMONS = 151;
+  const pokemonID = new URLSearchParams(window.location.search).get("id");
+  const id = parseInt(pokemonID, 10);
 
-    if (invalidId(id)) {
-        return (window.location.href = "./index.html");
-    }
+  if (id < 1 || id > MAX_POKEMONS) {
+    return (window.location.href = "./index.html");
+  }
 
-    currentPokemonId = id;
-    loadPokemon(id);
+  currentPokemonId = id;
+  loadPokemon(id);
 });
 
 async function loadPokemon(id) {
-    try {
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+        res.json()
+      ),
+    ]);
 
-        const [pokemon, pokemonSpecies] =  await Promise
-        .all([
-            // Fetch by id
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-            .then(
-                (res) => {
-                    res.json()
-                }
-            ),
-            // Fetch species
-            fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
-            .then(
-                (res) => {
-                    res.json()
-                }
-            ),
-        ]);
+    const abilitiesWrapper = document.querySelector(
+      ".pokemon-detail-wrap .pokemon-detail.move"
+    );
+    abilitiesWrapper.innerHTML = "";
 
-        const abilitiesWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail-move");
-        // Clean the data of the previously fetched pokeon
-        abilitiesWrapper.innerHTML = "";
+    if (currentPokemonId === id) {
+      displayPokemonDetails(pokemon);
+      const flavorText = getEnglishFlavorText(pokemonSpecies);
+      document.querySelector(".body3-fonts.pokemon-description").textContent =
+        flavorText;
 
-        if(currentPokemonId === id) {
-            displayPokemonDetails(pokemon);
-            const flavorText = getEnglishFlavorText(pokemonSpecies);
-            document.querySelector(".body3-fonts.pokemon-description").textContent = flavorText;
-            
-        }
+      const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map((sel) =>
+        document.querySelector(sel)
+      );
+      leftArrow.removeEventListener("click", navigatePokemon);
+      rightArrow.removeEventListener("click", navigatePokemon);
 
-        const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map(
-            (sel) => {
-                document.querySelector(sel);
-            }
-        );
-        leftArrow.removeEventListener("click", navigatePokemon);
-        rightArrow.removeEventListener("click", navigatePokemon);
+      if (id !== 1) {
+        leftArrow.addEventListener("click", () => {
+          navigatePokemon(id - 1);
+        });
+      }
+      if (id !== 151) {
+        rightArrow.addEventListener("click", () => {
+          navigatePokemon(id + 1);
+        });
+      }
 
-        if(id !== 1) {
-            leftArrow.addEventListener("click", 
-                () => {
-                    navigatePokemon(id-1);
-                }
-            );
-        }
-        if(id !== MAX_POKEMON) {
-            leftArrow.addEventListener("click", 
-                () => {
-                    navigatePokemon(id+1);
-                }
-            );
-        }
-
-        // Change the url without reloading the page
-        window.history.pushState({}, "", `./detail.html?id=${id}`);
-
-        return true;
+      window.history.pushState({}, "", `./detail.html?id=${id}`);
     }
-    catch(error) {
-        console.error("An error occurred while fetching pokemon data", error);
-        return false;
-    } 
+
+    return true;
+  } catch (error) {
+    console.error("An error occured while fetching Pokemon data:", error);
+    return false;
+  }
 }
 
 async function navigatePokemon(id) {
-    currentPokemonId = id;
-    await loadPokemon(id);
+  currentPokemonId = id;
+  await loadPokemon(id);
 }
 
-// Color background for every pokemon type
 const typeColors = {
-    normal: "#A8A878",
-    fire: "#F08030",
-    water: "#6890F0",
-    electric: "#F8D030",
-    grass: "#78C850",
-    ice: "#98D8D8",
-    fighting: "#C03028",
-    poison: "#A040A0",
-    ground: "#E0C068",
-    flying: "#A890F0",
-    psychic: "#F85888",
-    bug: "#A8B820",
-    rock: "#B8A038",
-    ghost: "#705898",
-    dragon: "#7038F8",
-    dark: "#705848",
-    steel: "#B8B8D0",
-  };
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  dark: "#EE99AC",
+};
 
-// Takes an html element and sets the desired cssProperty to a desired value
 function setElementStyles(elements, cssProperty, value) {
-
-    elements.forEach(
-        (element) => {
-            element.style[cssProperty] = value;
-        }
-    )
-
+  elements.forEach((element) => {
+    element.style[cssProperty] = value;
+  });
 }
 
-// Transforms a color to translucid
 function rgbaFromHex(hexColor) {
-    return [
-        parseInt(hexColor.slice(1, 3), 16),
-        parseInt(hexColor.slice(3, 5), 16),
-        parseInt(hexColor.slice(5, 7), 16)
-    ].join(", ");
-    
-    
+  return [
+    parseInt(hexColor.slice(1, 3), 16),
+    parseInt(hexColor.slice(3, 5), 16),
+    parseInt(hexColor.slice(5, 7), 16),
+  ].join(", ");
 }
 
 function setTypeBackgroundColor(pokemon) {
-    const mainType = pokemon.types[0].type.name;
-    const color = typeColors[mainType];
+  const mainType = pokemon.types[0].type.name;
+  const color = typeColors[mainType];
 
-    if(!color) {
-        console.warn(`Color not defined for type: ${mainType}`);
-        return;
+  if (!color) {
+    console.warn(`Color not defined for type: ${mainType}`);
+    return;
+  }
+
+  const detailMainElement = document.querySelector(".detail-main");
+  setElementStyles([detailMainElement], "backgroundColor", color);
+  setElementStyles([detailMainElement], "borderColor", color);
+
+  setElementStyles(
+    document.querySelectorAll(".power-wrapper > p"),
+    "backgroundColor",
+    color
+  );
+
+  setElementStyles(
+    document.querySelectorAll(".stats-wrap p.stats"),
+    "color",
+    color
+  );
+
+  setElementStyles(
+    document.querySelectorAll(".stats-wrap .progress-bar"),
+    "color",
+    color
+  );
+
+  const rgbaColor = rgbaFromHex(color);
+  const styleTag = document.createElement("style");
+  styleTag.innerHTML = `
+    .stats-wrap .progress-bar::-webkit-progress-bar {
+        background-color: rgba(${rgbaColor}, 0.5);
     }
-
-    const detailMainElement = document.querySelector(".detail-main");
-    setElementStyles(detailMainElement, "background-color", color);
-    setElementStyles(detailMainElement, "border-color", color);
-
-    setElementsStyles(document.querySelectorAll(".power-wrapper > p"), "background-color", color);
-    setElementsStyles(document.querySelectorAll(".stats-wrap p.stats"), "color", color);
-    setElementsStyles(document.querySelectorAll(".stats-wrap .progress-bar"), "color", color);
-
-    const rgbaColor = rgbaFromHex(color);
-    const styleTag = document.createElement("style");
-    styleTag.innerHTML = `
-        .stats-wrap .progress-bar::-webkit-progress-bar {
-            background-color: rgba(${rgbaColor}, 0.5);
-        }
-
-        .stats-wrap .progress-bar::-webkit-progress-value {
-            background-color: ${color};
-        }
-    `;
-
-    document.head.appendChild(styleTag);
+    .stats-wrap .progress-bar::-webkit-progress-value {
+        background-color: ${color};
+    }
+  `;
+  document.head.appendChild(styleTag);
 }
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-function createAndAppendElement(parent, tag, options={}) {
-    const element = document.createElement(tag);
-    Object.keys.forEach(
-        (key) => {
-            element[key] = options[key];
-        }
-    );
-    parent.appendChild(element);
-    return element;
+function createAndAppendElement(parent, tag, options = {}) {
+  const element = document.createElement(tag);
+  Object.keys(options).forEach((key) => {
+    element[key] = options[key];
+  });
+  parent.appendChild(element);
+  return element;
 }
 
 function displayPokemonDetails(pokemon) {
-    const {name, id, types, weight, height, abilities, stats} = pokemon;
-    const capitalizePokemonName = capitalizeFirstLetter(name);
-    
-    document.querySelectorAll("title").textContent = capitalizePokemonName;
+  const { name, id, types, weight, height, abilities, stats } = pokemon;
+  const capitalizePokemonName = capitalizeFirstLetter(name);
 
-    const detailMainElement = document.querySelector(".detail-main");
-    detailMainElement.classList.add(name.toLowerCase());
+  document.querySelector("title").textContent = capitalizePokemonName;
 
-    document.querySelector(".   name-wrap .name").textContent = capitalizePokemonName;
+  const detailMainElement = document.querySelector(".detail-main");
+  detailMainElement.classList.add(name.toLowerCase());
 
-    document.querySelector(".pokemon-id-wrap .body2-fonts").textContent = `#${String(id).padStart(3, "0")}`;
+  document.querySelector(".name-wrap .name").textContent =
+    capitalizePokemonName;
 
-    const imageElement = document.querySelector(".detail-img-wrapper img");
-    imageElement.src = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/other/dream-world/${id}.svg`;
+  document.querySelector(
+    ".pokemon-id-wrap .body2-fonts"
+  ).textContent = `#${String(id).padStart(3, "0")}`;
 
-    const typeWrapper = document.querySelector(".power-wrapper");
-    typeWrapper.innerHTML = "";
-    types.forEach(
-        (
-            {type}
-        ) => {
-            createAndAppendElement(typeWrapper, "p", {
-                className: `body3-fonts type ${type.name}`,
-                textContent: type.name,
-            }
-        );
-        }
-    );
+  const imageElement = document.querySelector(".detail-img-wrapper img");
+  imageElement.src = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+  imageElement.alt = name;
 
-    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.weight").textContent = `${weight / 10} kg`;
-    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.height").textContent = `${height / 10} m`;
+  const typeWrapper = document.querySelector(".power-wrapper");
+  typeWrapper.innerHTML = "";
+  types.forEach(({ type }) => {
+    createAndAppendElement(typeWrapper, "p", {
+      className: `body3-fonts type ${type.name}`,
+      textContent: type.name,
+    });
+  });
 
-    const abilitiesWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail-move");
-    abilities.forEach (
-        (
-            {ability}
-        ) => {
-            createAndAppendElement(abilitiesWrapper, "p", {
-                className: "body3-fonts",
-                textContent: ability.name,
-            });
-        }
-    );
+  document.querySelector(
+    ".pokemon-detail-wrap .pokemon-detail p.body3-fonts.weight"
+  ).textContent = `${weight / 10}kg`;
+  document.querySelector(
+    ".pokemon-detail-wrap .pokemon-detail p.body3-fonts.height"
+  ).textContent = `${height / 10}m`;
 
-    const statsWrapper = document.querySelector("stats-wrapper");
-    statsWrapper.innerHTML = "";
+  const abilitiesWrapper = document.querySelector(
+    ".pokemon-detail-wrap .pokemon-detail.move"
+  );
+  abilities.forEach(({ ability }) => {
+    createAndAppendElement(abilitiesWrapper, "p", {
+      className: "body3-fonts",
+      textContent: ability.name,
+    });
+  });
 
-    const statsNameMapping = {
-        hp: "HP",
-        attack: "ATK",
-        defense: "DEF",
-        "special-attack": "SATK",
-        "special-defense": "SDEF",
-        speed: "SPD",
-    };
+  const statsWrapper = document.querySelector(".stats-wrapper");
+  statsWrapper.innerHTML = "";
 
-    stats.forEach(({stat, base_stat}) => {
-        const statDiv = document.createElement("div");
-        statDiv.className = "stats-wrap";
-        statsWrapper.appendChild(statDiv);
+  const statNameMapping = {
+    hp: "HP",
+    attack: "ATK",
+    defense: "DEF",
+    "special-attack": "SATK",
+    "special-defense": "SDEF",
+    speed: "SPD",
+  };
 
-        createAndAppendElement(statDiv, "p", {
-            className: "body3-fonts stats",
-            textContent: statsNameMapping[stat.name],
-        });
+  stats.forEach(({ stat, base_stat }) => {
+    const statDiv = document.createElement("div");
+    statDiv.className = "stats-wrap";
+    statsWrapper.appendChild(statDiv);
 
-        createAndAppendElement(statDiv, "p", {
-            className: "body3-fonts",
-            textContent: String(base_stat).padStart(3, "0"),
-        });
-
-        createAndAppendElement(statDiv, "progress", {
-            className: "progress-bar",
-            value: base_stat,
-            max: 100,
-        });
+    createAndAppendElement(statDiv, "p", {
+      className: "body3-fonts stats",
+      textContent: statNameMapping[stat.name],
     });
 
-    setTypeBackgroundColor(pokemon);
+    createAndAppendElement(statDiv, "p", {
+      className: "body3-fonts",
+      textContent: String(base_stat).padStart(3, "0"),
+    });
 
+    createAndAppendElement(statDiv, "progress", {
+      className: "progress-bar",
+      value: base_stat,
+      max: 100,
+    });
+  });
+
+  setTypeBackgroundColor(pokemon);
 }
 
 function getEnglishFlavorText(pokemonSpecies) {
-    for (let entry of pokemonSpecies.flavor_text_entries) {
-        if(entry.language.name === "en") {
-            let flavor = entry.flavor_text.replace(/\f/g, "");
-            return flavor;
-        }
+  for (let entry of pokemonSpecies.flavor_text_entries) {
+    if (entry.language.name === "en") {
+      let flavor = entry.flavor_text.replace(/\f/g, " ");
+      return flavor;
     }
-
-    return ""; 
-}
-
-
-function invalidId(id) {
-    return id < 1 || id > MAX_POKEMONS;
+  }
+  return "";
 }
